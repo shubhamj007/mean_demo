203c669cf450b97e9205bbd982525299753b4035
dhanashri.p <dhanashri@intelehealth.org>
2021-06-30 19:09:05 +0530

    login related changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CoreModule,
     AuthModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component'
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 const routes: Routes = [
+{
+  path: '',
+  pathMatch: 'full',
+  component: LoginComponent,
+  data: { title: 'Log In' }
+},  
 {
   path: 'login',
   component: LoginComponent,
@@ -32,7 +40,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule,
+    AngularMaterialModule,
+    FlexLayoutModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AuthRoutingModule {
   static components = [
